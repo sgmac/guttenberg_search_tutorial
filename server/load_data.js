a99a0f780756c781819bcba3a65b6e12877de5a3
Sergio Galvan <sergalma@gmail.com>
2018-04-01 14:26:18 +0200

    (backend): define functions to load data into ES

---
+++ server/load_data.js
@@ -0,0 +1,57 @@
+const fs = require('fs')
+const path = require('path')
+const esConnection = require('./connection')
+
+/** Clear ES index, parse and index all files from the books directory */
+async function readAndInsertBooks () {
+  try {
+    // Clear previous ES index
+    await esConnection.resetIndex()
+
+    // Read books directory
+    let files = fs.readdirSync('./books').filter(file => file.slice(-4) === '.txt')
+    console.log(`Found ${files.length} Files`)
+
+    // Read each book file, and index each paragraph in elasticsearch
+    for (let file of files) {
+      console.log(`Reading File - ${file}`)
+      const filePath = path.join('./books', file)
+      const { title, author, paragraphs } = parseBookFile(filePath)
+      await insertBookData(title, author, paragraphs)
+    }
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+
+/** Read an individual book text file, and extract the title, author, and paragraphs */
+function parseBookFile (filePath) {
+  // Read text file
+  const book = fs.readFileSync(filePath, 'utf8')
+
+  // Find book title and author
+  const title = book.match(/^Title:\s(.+)$/m)[1]
+  const authorMatch = book.match(/^Author:\s(.+)$/m)
+  const author = (!authorMatch || authorMatch[1].trim() === '') ? 'Unknown Author' : authorMatch[1]
+
+  console.log(`Reading Book - ${title} By ${author}`)
+
+  // Find Guttenberg metadata header and footer
+  const startOfBookMatch = book.match(/^\*{3}\s*START OF (THIS|THE) PROJECT GUTENBERG EBOOK.+\*{3}$/m)
+  const startOfBookIndex = startOfBookMatch.index + startOfBookMatch[0].length
+  const endOfBookIndex = book.match(/^\*{3}\s*END OF (THIS|THE) PROJECT GUTENBERG EBOOK.+\*{3}$/m).index
+
+  // Clean book text and split into array of paragraphs
+  const paragraphs = book
+    .slice(startOfBookIndex, endOfBookIndex) // Remove Guttenberg header and footer
+    .split(/\n\s+\n/g) // Split each paragraph into it's own array entry
+    .map(line => line.replace(/\r\n/g, ' ').trim()) // Remove paragraph line breaks and whitespace
+    .map(line => line.replace(/_/g, '')) // Guttenberg uses "_" to signify italics.  We'll remove it, since it makes the raw text look messy.
+    .filter((line) => (line && line !== '')) // Remove empty lines
+
+  console.log(`Parsed ${paragraphs.length} Paragraphs\n`)
+  return { title, author, paragraphs }
+}
+
+readAndInsertBooks()
